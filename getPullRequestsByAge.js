98c6101f7d70a1c3b301222a755ce7bc3b6276b2
Christian Dittrich <christian.dittrich@kaiserkraft-europa.de>
2017-10-29 12:48:30 +0100

    Refactor getPullRequestsByAge

--- getPullRequestsByAge.js
+++ getPullRequestsByAge.js
@@ -2,19 +2,39 @@ const R = require('ramda');
 const viewValues = require('./viewValues.js');
 
 /**
+ * @typedef {Object} PullRequestWithRepo
+ * @extends PullRequest
+ * @property {string} repo
+ */
+
+/**
+ * @param {PullRequest} pullRequest
+ * @returns {{title: string, id: string, createdDate: number, repo: string}}
+ */
+const pickInfo = R.pick(['title', 'id', 'createdDate', 'repo']);
+
+/**
+ * @param {PullRequest} pullRequest
+ * @returns {string}
+ */
+const viewRepoName = R.view(R.lensPath(['fromRef', 'repository', 'name']));
+
+/**
+ * @param {PullRequest} pR
+ * @returns {PullRequestWithRepo}
+ */
+const addRepoName = pR => R.assoc('repo', viewRepoName(pR), pR);
+
+/**
  * @param {Array.<RepoPullRequests>} repoPullRequests A list of all pull requests for all repositories
  * @return {Array.<MinimalPullRequestInfo>}
  */
-const getPullRequestsByAge = repoPullRequests => {
-    const pullRequests = R.flatten(R.map(viewValues, repoPullRequests));
-    const ps = R.map(pR => R.assoc('repo', pR.fromRef.repository.name, pR), pullRequests);
-    const pRs = R.map(R.pick([
-        'title',
-        'id',
-        'createdDate',
-        'repo'
-    ]), ps);
-    return R.sortBy(R.prop('createdDate'), pRs);
-};
+const getPullRequestsByAge = R.compose(
+    R.sortBy(R.prop('createdDate')),
+    R.map(pickInfo),
+    R.map(addRepoName),
+    R.flatten,
+    R.map(viewValues)
+);
 
 module.exports = getPullRequestsByAge;
